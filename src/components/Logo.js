a8db83f208f67f77bea1d6987699d35f302286ba
Omar J <okcomputer2393@gmail.com>
2021-07-03 02:35:43 -0500

    add responsive design to completed pledge modal component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { bpSmall } from '../styles/breakpoints';
 
 const StyledTitle = styled.h1`
   font-size: 2.5rem;
   font-weight: 700;
   letter-spacing: -1px;
   color: var(--white);
+
+  @media only screen and (max-width: ${bpSmall}) {
+    font-size: 4rem;
+  }
 
   .title__link {
     color: var(--white);
--- src/components/modal/CompletedPledgeModal.js
+++ src/components/modal/CompletedPledgeModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Modal from './Modal';
 import Button from '../Button';
+import { bpSmall } from '../../styles/breakpoints';
 import iconCheck from '../../assets/images/icon-check.svg';
 
 const StyledModalContent = styled.div`
@@ -11,17 +12,39 @@ const StyledModalContent = styled.div`
   align-items: center;
   width: 41.5rem;
 
+  @media only screen and (max-width: ${bpSmall}) {
+    width: 100%;
+  }
+
   .completed-pledge__icon-check {
   }
 
   .completed-pledge__title {
     text-align: center;
     margin-top: 2rem;
+
+    @media only screen and (max-width: ${bpSmall}) {
+      margin-top: 3rem;
+      font-size: 3rem;
+    }
   }
 
   .completed-pledge__desc {
     text-align: center;
     margin: 1rem 0 3rem;
+
+    @media only screen and (max-width: ${bpSmall}) {
+      font-size: 2.3rem !important;
+      margin: 4rem 0;
+      font-weight: 400;
+    }
+  }
+
+  .completed-pledge__button {
+    @media only screen and (max-width: ${bpSmall}) {
+      font-size: 2rem;
+      padding: 2rem 4rem;
+    }
   }
 `;
 
@@ -32,7 +55,9 @@ const CompletedPledgeModal = ({ isOpen, closeModal, product }) => {
         <img src={iconCheck} alt="icon check" className="completed-pledge__icon-check" />
         <h4 className="completed-pledge__title title-secondary">Thanks for your support!</h4>
         <p className="completed-pledge__desc text-secondary--normal">{`Your pledge bring us one step closer to sharing ${product} worldwide. You will get an email once our campaign is completed.`}</p>
-        <Button size="medium">Got it!</Button>
+        <Button size="medium" onClick={closeModal} className="completed-pledge__button">
+          Got it!
+        </Button>
       </StyledModalContent>
     </Modal>
   );
